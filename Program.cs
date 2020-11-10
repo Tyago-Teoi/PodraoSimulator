f61b9d14fc4fae6cd3853c71cc2d466b45998359
7Nic <gabriel.nicolau97@hotmail.com>
2020-11-10 17:34:37 -0200

    Example producer consumer for threads

--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@ namespace Program
     class Teste {
         static Thread[] t = new Thread[5];
         static Semaphore semaphore = new Semaphore(2,2);
+
+        static Thread producerThread;
+        static Thread consumerThread;
+        // static Semaphore empty = new Semaphore(10, 10);
+        // static Semaphore full = new Semaphore(0, 10);
+        static SemaphoreSlim empty = new SemaphoreSlim(10, 10);
+        static SemaphoreSlim full = new SemaphoreSlim(0, 10);
+        static Semaphore mutex = new Semaphore(1, 1);
         static void DoThing() {
             Console.WriteLine("{0} = waiting", Thread.CurrentThread.Name);
             semaphore.WaitOne();
@@ -15,13 +23,43 @@ namespace Program
             semaphore.Release();
         }
 
-        static void Main(string[] args) {
-            for (int i=0; i<5; i++) {
-                t[i] = new Thread(DoThing);
-                t[i].Name = "Thread de número: " + i;
-                t[i].Start();
+        static void Producer() {
+            while(true) {
+                Console.WriteLine("Producing item...");
+                Console.WriteLine(empty.CurrentCount + " empty items on buffer");
+                empty.Wait();
+                mutex.WaitOne();
+                Thread.Sleep(500);
+                Console.WriteLine("Putting item in the buffer...");
+                mutex.Release();
+                full.Release();
             }
-            Console.Read();
+        }
+
+        static void Consumer() {
+            while(true) {
+                Console.WriteLine("Consuming item...");
+                Console.WriteLine(full.CurrentCount + " used items on buffer");
+                full.Wait();
+                mutex.WaitOne();
+                Console.WriteLine("Removing item from buffer...");
+                mutex.Release();
+                empty.Release();
+            }
+        }
+
+        static void Main(string[] args) {
+            // for (int i=0; i<5; i++) {
+            //     t[i] = new Thread(DoThing);
+            //     t[i].Name = "Thread de número: " + i;
+            //     t[i].Start();
+            // }
+            // Console.Read();
+
+            producerThread = new Thread(Producer);
+            consumerThread = new Thread(Consumer);
+            producerThread.Start();
+            consumerThread.Start();
         }
     }
 }
