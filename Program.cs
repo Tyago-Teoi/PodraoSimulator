07112d46d07caaed517afaaba5a570772463595b
7Nic <gabriel.nicolau97@hotmail.com>
2020-11-10 17:10:35 -0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+namespace Program
+
+{
+    class Teste {
+        static Thread[] t = new Thread[5];
+        static Semaphore semaphore = new Semaphore(2,2);
+        static void DoThing() {
+            Console.WriteLine("{0} = waiting", Thread.CurrentThread.Name);
+            semaphore.WaitOne();
+            Console.WriteLine("{0} begins!", Thread.CurrentThread.Name);
+            Thread.Sleep(1000);
+            Console.WriteLine("{0} releasing...", Thread.CurrentThread.Name);
+            semaphore.Release();
+        }
+
+        static void Main(string[] args) {
+            for (int i=0; i<5; i++) {
+                t[i] = new Thread(DoThing);
+                t[i].Name = "Thread de número: " + i;
+                t[i].Start();
+            }
+            Console.Read();
+        }
+    }
+}
